4bfe95de2992b2e6365a49cc021b2a4aa87df173
Rob <rob.gldstein13@gmail.com>
2020-04-28 13:30:25 -0400

    Fix CORs issue

--- Code Tests/single-stop/single-stop-mock-comment-api/app.js
+++ Code Tests/single-stop/single-stop-mock-comment-api/app.js
@@ -9,6 +9,12 @@ var commentsRouter = require('./routes/comments');
 
 var app = express();
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
